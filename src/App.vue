2305dce1c1aad1017535becb1497716dc8efce41
root <root@vultr.guest>
2023-09-12 19:32:00 +0000

    info

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,9 @@
   <div>
     <router-view/>
   </div>
+  <p>欢迎伙伴们填写友善的信息~若有需要修改的内容和改进的建议，请联系版权人：amicable.friendly@gmail.com</p>
+  <p>友善信息库©版权所有</p>
+  <a href="https://beian.miit.gov.cn" target="_blank">京ICP备2023017790号-1</a>
 </template>
 
 <script>
